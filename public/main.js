42380d555c171a7e22078253810686c5585608e1
King <70107167+Programador-jr@users.noreply.github.com>
2024-03-17 00:07:00 +0000

    music fix

--- public/main.js
+++ public/main.js
@@ -1,3 +1,8 @@
 onload = () =>{
         document.body.classList.remove("container");
 };
+
+   window.addEventListener('beforeunload', function(event) {
+        var audio = document.getElementById('music');
+        audio.pause(); // Pausa a reprodução do áudio
+    });
