52986be5599d7f243578485b6e076ceeda802785
King <70107167+Programador-jr@users.noreply.github.com>
2024-05-07 08:18:59 +0000

    added flowers
    flowers added with a shade of bood red

--- index.js
+++ index.js
@@ -1,27 +1,27 @@
-const express = require('express');
-const path  = require('path');
-const bcrypt = require('bcrypt')
+const express = require("express");
+const path = require("path");
+const bcrypt = require("bcrypt");
 
-let staticPath = path.join(__dirname, "public")
+let staticPath = path.join(__dirname, "public");
 
 const app = express();
 
 app.use(express.static(staticPath));
 
-app.listen(8000, () => {
-	console.log('Executando na porta 8080...');
-})
-
-
+app.listen(80, () => {
+	console.log("Executando na porta 80...");
+});
 
 app.get("/", (req, res) => {
 	res.sendFile(path.join(staticPath, "index.html"));
-})
+});
 
-app.get("/404", (req, res) => {
+/**
+ app.get("/404", (req, res) => {
 	res.sendFile(path.join(staticPath, "404.html"));
 })
 
 app.use((req, res) => {
 	res.redirect('/404');
 })
+*/
--- public/main.js
+++ public/main.js
@@ -1,8 +1,8 @@
-onload = () =>{
+onload = () => {
         document.body.classList.remove("container");
 };
 
-   window.addEventListener('beforeunload', function(event) {
-        var audio = document.getElementById('music');
+window.addEventListener("beforeunload", function (event) {
+        var audio = document.getElementById("music");
         audio.pause(); // Pausa a reprodução do áudio
-    });
+});
